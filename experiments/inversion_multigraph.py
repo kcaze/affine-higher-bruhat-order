e9435f30006dcdcb258f9811bc00b35675538d80
Herman Chau <beijingherman@gmail.com>
2025-03-14 09:39:47 -0700

    Add sufficiency test.

--- experiments/inversion_multigraph.py
+++ experiments/inversion_multigraph.py
@@ -18,11 +18,21 @@ def multigraphs_satisfying_triangle_inequality(n, max_weight):
         for i in range(n):
             for j in range(i+1, n):
                 for k in range(j+1, n):
+                    ik = 1 if G[i][k] >= 0 else -1
+                    ij = 1 if G[i][j] >= 0 else -1
+                    jk = 1 if G[j][k] >= 0 else -1
+                    if (ik,ij,jk) in [(-1,1,1), (1,-1,-1)]:
+                        is_valid = False
+
                     A = i if G[i][j] >= 0 and G[i][k] >= 0 else j if G[j][i] >= 0 and G[j][k] >= 0 else k
                     C = k if G[j][k] >= 0 and G[i][k] >= 0 else j if G[i][j] >= 0 and G[k][j] >= 0 else i
                     B = [x for x in [i,j,k] if x != A and x != C][0]
-                    if abs(G[A][B] - G[A][C] + G[B][C]) > 1:
-                        is_valid = False
+                    if (A,B,C) in [(i,j,k), (k,i,j), (j,k,i)]:
+                        if G[A][B] - G[A][C] + G[B][C] not in [0, -1]:
+                            is_valid = False
+                    else:
+                        if G[A][B] - G[A][C] + G[B][C] not in [0, 1]:
+                            is_valid = False
         if is_valid:
             yield tuple(tuple(x) for x in G)
 
@@ -48,16 +58,17 @@ def experiment_difference_values():
     return difference_values
 
 def experiment_all_triangle_equality_graphs_exist():
-    n = 3
+    n = 4
     expected = set(multigraphs_satisfying_triangle_inequality(n,2))
     actual = set()
-    for l in tqdm(range(10)):
+    for l in tqdm(range(11)):
         for w in permutations(n, l):
             actual.add(inversion_multigraph(w))
     return actual, expected
 
-print(experiment_difference_values())
+# print(experiment_difference_values())
 
-# actual, expected = experiment_all_triangle_equality_graphs_exist()
-# print(expected - actual)
-# print(len(list(multigraphs_satisfying_triangle_inequality(3,2))))
+actual, expected = experiment_all_triangle_equality_graphs_exist()
+print(expected - actual)
+print(len(expected))
+print(len(actual))
