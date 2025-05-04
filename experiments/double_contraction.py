82dc49f79163fd8ba7da2a1f3e6455df0273238b
Herman Chau <beijingherman@gmail.com>
2025-05-04 16:43:07 -0400

    Update with generalized_truncation

--- experiments/double_contraction.py
+++ experiments/double_contraction.py
@@ -1,13 +1,15 @@
 import itertools
 from lib import *
 
-for w in itertools.permutations([1,2,3,4,5]):
+for w in itertools.permutations([1,2,3,4,5,6]):
     n = len(w)
     for R in consistent_sets(w, n-1):
         G_R = G(w, n-1, R)
         for X in G_R:
             for Y in G_R[X]["outs"]:
                 if X == Y:
+                    continue
+                if n in X or n in Y:
                     continue
                 for j in range(1, n-1):
                     if (j in X and j in Y) and (j+1 in X and j+1 in Y):
---
+++ experiments/generalized_truncation.py
@@ -0,0 +1,4 @@
+from lib import *
+
+print(generalized_truncation((-2, -3, 11, 5, 4)))
+print(generalized_truncation((2, 5, 6, 3, 4, 1)))
